18f68bb38d4ca40219d81ca011fc1585d1db3c2c
B Ricks <bricks@unomaha.edu>
2019-05-28 08:48:01 -0500

    Fix module.exports

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-const Command = require("./Command.js");
-const CommandExecutor = require("./CommandExecutor.js");
+class Command{
+  constructor(){
+    this.name = "<UNDEFINED COMMAND NAME>";
+    this.includeWithPrevious = false; //for multiple commands that should be counted as one.
+  }
 
-module.exports = {Command, CommandExecutor};
+  do(){
+    throw "do() method must be overwritten in child class.";
+  }
+  undo(){
+    throw "undo() method must be overwritten in child class.";
+  }
+ 
+}
+
+class CommandExecutor {
+  constructor() {
+    this.didStack = [];
+    this.undidStack = [];
+  }
+  do(command) {
+    this.undidStack = [];
+    this.didStack.push(command);
+    command.do();
+  }
+  undo() {
+    let toUndo = {};
+    do {
+      if (this.didStack.length == 0) return;
+      toUndo = this.didStack.splice(this.didStack.length - 1, 1)[0];
+      toUndo.undo();
+      this.undidStack.push(toUndo);
+    } while (toUndo.includeWithPrevious == true);
+  }
+  redo() {
+    let toRedo = {};
+
+    if (this.undidStack.length == 0) return;
+    toRedo = this.undidStack.splice(this.undidStack.length - 1, 1)[0];
+    toRedo.do();
+    this.didStack.push(toRedo);
+
+    //Now check to see if the following ones can also be redone
+    while (this.undidStack.length != 0 && this.undidStack[this.undidStack.length - 1].includeWithPrevious) {
+      toRedo = this.undidStack.splice(this.undidStack.length - 1, 1)[0];
+      toRedo.do();
+      this.didStack.push(toRedo);
+    }
+
+
+  }
+  canUndo() {
+    return this.didStack.length != 0;
+  }
+  canRedo() {
+    return this.undidStack.length != 0;
+  }
+}
+
+
+
+module.exports = {
+  Command, CommandExecutor
+};
