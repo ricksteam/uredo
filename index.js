79d3fdd8fc571c7394782abd6c2161263d3b411c
B Ricks <bricks@unomaha.edu>
2019-05-28 10:27:40 -0500

    Add api to readme

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+/**
+ * Class that implements an undo/redo stack.
+ * To use, pass an object to the do function that has at least two paramaterless functions, do() and undo().
+ * If the object have a member variable includeWithPrevious set to true, the undo and redo stack will treat 
+ * command as an incremental command that should be done/redone with the previous command.
+ */
 export default class CommandExecutor {
   constructor() {
     this.didStack = [];
     this.undidStack = [];
   }
+  /**
+   * 
+   * Execute a command and place it on the undo stack. Also clears the redo stack()
+   * 
+   * @param {object} command An object with two parameterless functions, do() and undo()
+   */
   do(command) {
     this.undidStack = [];
     this.didStack.push(command);
     command.do();
   }
+
+  /**
+   * Pop the last item on the undo stack, call its undo function, and push it on the redo stack. 
+   * Throws an error if there is no undo stack.
+   * If the last object was the includeWithPrevious flag set, it will undo the previous command
+   * until the undo stack is empty or a command is found without the includeWithPrevious or a command
+   * with that flag set to false.
+   */
   undo() {
     let toUndo = {};
     do {
@@ -17,6 +37,13 @@ export default class CommandExecutor {
       this.undidStack.push(toUndo);
     } while (toUndo.includeWithPrevious == true);
   }
+  /**
+   * Pop the last item off the redo stack, call its do() functions, and push it on the undo stack.
+   * Throws an error if there is no redo stack.
+   * If the following object(s) have the includeWithPrevious flag set, it will redo the following commands
+   * until the redo stack is empty or a command is found with the includeWithPrevious flag or a command 
+   * with that flag set to false.
+   */
   redo() {
     let toRedo = {};
 
@@ -34,9 +61,15 @@ export default class CommandExecutor {
 
 
   }
+  /**
+   * Check to see if the undo stack has at least one object.
+   */
   canUndo() {
     return this.didStack.length != 0;
   }
+  /**
+   * Check to see if the redo stack has at least one object.
+   */
   canRedo() {
     return this.undidStack.length != 0;
   }
